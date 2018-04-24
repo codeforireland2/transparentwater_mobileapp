ed75df4de618d78b9205e1d80dc52258a552d6fd
SmilingKat <katja.rademacher@pikk.net>
2018-04-24 21:19:18 +0100

    Added a simple map display
    The center of the map display is Dublin

--- TwMapView.js
+++ TwMapView.js
@@ -1,17 +1,43 @@
 import React from 'react';
 import { StyleSheet, Dimensions, View } from 'react-native';
+import MapView from 'react-native-maps';
+
 
 export class TwMapView extends React.Component {
+	
+	state = {
+		region: {
+			latitude:  53.350140,
+			longitude: -6.266155,
+			latitudeDelta: 0.2,
+			longitudeDelta: 0.2
+		}
+    }
+	
+	onRegionChange (region) {
+		this.setState({ region });
+	}
+
+
 	render() {
 		return (
-			  <View />
+			<View style={styles.container}>
+				<MapView
+					style={styles.map}
+					initialRegion={this.state.region}
+					onRegionChange={this.onRegionChange}
+					showsUserLocation={true}
+				/>
+			</View>
 		);
 	}
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-	backgroundColor: '#673ab7'
+	container: {
+		flex: 1
+	},
+  map: {
+    flex: 1
   },
 });
