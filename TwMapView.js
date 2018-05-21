2b4f8d696e72bd2b3e7d246448c61f917032efb9
SmilingKat <katja.rademacher@pikk.net>
2018-05-21 22:25:38 +0100

    Map Loading by shown area
    filter notice data for items only within the currently shown region, by
    checking boundries

--- TwMapView.js
+++ TwMapView.js
@@ -12,6 +12,10 @@ const styles = StyleSheet.create({
   }
 });
 
+/**
+* @class TwMapView
+* display of data on a map
+*/
 export class TwMapView extends React.Component {
   state = {
     region: {
@@ -19,23 +23,79 @@ export class TwMapView extends React.Component {
       longitude: -6.266155,
       latitudeDelta: 0.2,
       longitudeDelta: 0.2
-    }
+    },
+    markers: []
   }
 
+  /**
+  * @function componentDidMount
+  * function that is called after the component is initialised
+  * this calculates the initial list of markers
+  */
+  componentDidMount() {
+    this._findLocalMarkers();
+  }
+
+  /**
+  * @function onRegionChange
+  * this gets called when the user moves the map
+  */
   onRegionChange(region) {
-    this.setState({ region });
+    this.setState({ region: region });
+    this._findLocalMarkers();
   }
 
+  /**
+  * @function _findLocalMarkers
+  * calculates the maximum/minimum points for the screen display
+  * only adds notices to the list if they are within these boundaries
+  * this increases loading speed
+  * whenever the setState is called and the markers item is updated,
+  * the markers shown on the map are updated automatically
+  */
+  _findLocalMarkers() {
+    const regionInfo = this.state.region;
+    const allNotices = this.props.data;
+    const localNotices = [];
+    const latDelta = regionInfo.latitudeDelta;
+    const lngDelta = regionInfo.longitudeDelta;
+    const maxLatitude = regionInfo.latitude + latDelta;
+    const minLatitude = regionInfo.latitude - latDelta;
+    const maxLongitude = regionInfo.longitude + lngDelta;
+    const minLongitude = regionInfo.longitude - lngDelta;
+
+    allNotices.forEach((notice) => {
+      const lat = notice.LAT;
+      const lng = notice.LONG;
+
+      if (lat > minLatitude &&
+        lat < maxLatitude &&
+        lng > minLongitude &&
+        lng < maxLongitude) {
+        localNotices.push(notice);
+      }
+    });
+
+    console.log('No of Markers: ', localNotices.length);
+    this.setState({
+      markers: localNotices
+    });
+  }
+
+  /**
+  * @function render
+  * renders the map and markers initially
+  */
   render() {
     return (
       <View style={styles.container}>
         <MapView
           style={styles.map}
           initialRegion={this.state.region}
-          onRegionChange={() => this.onRegionChange.bind(this)}
+          onRegionChangeComplete={(region) => { this.onRegionChange(region); }}
           showsUserLocation={true} // eslint-disable-line react/jsx-boolean-value
         >
-          {this.props.data.map(notice => (
+          {this.state.markers.map(notice => (
             <MapView.Marker
               key={notice.OBJECTID}
               coordinate={{
