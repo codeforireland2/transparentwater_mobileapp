3b422cb591afed9dcfacb7ccd8bce31136823069
MS <ms@MSs-MacBook-Pro.local>
2018-05-31 15:48:23 +0100

    adding description
    adding colors and description

--- TwListView.js
+++ TwListView.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { StyleSheet, ScrollView, Text, TextInput, View } from 'react-native';
-
+/*
+ * StyleSheet
+ * textInput for search bar style
+ * textBar between Title, Notice and Location
+*/
 const styles = StyleSheet.create({
   container: {
     borderWidth: 1.0,
     backgroundColor: 'rgba(247,247,247,1.0)',
-    paddingTop: 70,
     padding: 40,
     height: 44,
     flexDirection: 'column'
@@ -14,15 +17,40 @@ const styles = StyleSheet.create({
   textInput: {
     height: 30,
     borderWidth: 1,
-    borderRadius: 10,
+    borderRadius: 8,
     backgroundColor: 'transparent',
-    borderColor: 'skyblue',
-    fontWeight: 'bold'
+    borderColor: '#cecece',
+    marginBottom: 5,
+    marginTop: 15,
+    fontWeight: 'bold',
+    marginHorizontal: 4
+
   },
-  textBar: {
-    height: 10,
-    backgroundColor: 'powderblue'
-  }
+  top: {
+    height: 25,
+    backgroundColor: 'transparent',
+    borderRadius: 4,
+    justifyContent: 'space-between',
+    flex: 2,
+    flexDirection: 'row',
+    fontWeight: 'bold'
+
+  },
+  title: {
+    height: 25,
+    backgroundColor: 'steelblue',
+    fontWeight: 'bold'
+
+  },
+  text1: {
+    height: 28,
+    backgroundColor: 'skyblue',
+
+  },
+  text2: {
+    backgroundColor: 'powderblue',
+
+  },
 
 });
 // disabled for now, at least until we figure out if this
@@ -31,6 +59,8 @@ const styles = StyleSheet.create({
 /**
 * @class TwListView
 * display of the data in a list
+* the title is in bold
+* the View has a bar
 */
 export class TwListView extends React.Component {
   /**
@@ -40,17 +70,19 @@ export class TwListView extends React.Component {
     const props = this.props;
     return (
       <ScrollView style={styles.container}>
-        <TextInput style={styles.textInput} />
+        <TextInput
+          style={styles.textInput}
+          // onChangeText={(text) => this.filterSearch(text)}
+        />
         {props.data.map((item) => {
           console.log(item.TITLE);
-          console.log(item.LOCATION);
-          console.log(item.NOTICETYPE);
           return (
             <View>
-              <View style={styles.textBar} />
-              <Text style={{ fontWeight: 'bold' }}>{item.TITLE} </Text>
-              <Text >{item.NOTICETYPE} </Text>
-              <Text >{item.LOCATION} </Text>
+              <View style={styles.top} />
+              <Text style={styles.title} >{item.TITLE} </Text>
+              <Text style={styles.text1} > {item.LOCATION} </Text>
+              <Text style={styles.text1} > {item.NOTICETYPE} </Text>
+              <Text style={styles.text2} > {item.DESCRIPTION} </Text>
             </View>
           );
         })}
